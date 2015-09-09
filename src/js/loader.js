3b60b0b417fc0fd80bbf2d27011fa098f3927f4f
Joshua Wise <josh@joshuawise.ninja>
2015-09-09 01:08:10 -0400

    added progress module

--- src/js/loader.js
+++ src/js/loader.js
@@ -3,10 +3,24 @@
 var document = window.document;
 
 module.exports = function (Limber) {
-	function setLoaded(loaded) {
-		var loader = document.getElementById('limber-loader');
-		loader && loader.classList[loaded ? 'add' : 'remove']('loaded');
-	}
-	Limber.ready = setLoaded.bind(Limber, true);
-	Limber.loading = setLoaded.bind(Limber, false);
+	Limber.loader = new Loader;
 };
+
+function Loader() {
+	function setLoaded(loaded) {
+		var loaderEl = document.getElementById('limber-loader');
+		loaderEl && loaderEl.classList[loaded ? 'add' : 'remove']('loaded');
+	}
+	this.ready = setLoaded.bind(this, true);
+	this.loading = setLoaded.bind(this, false);
+	
+	var progress = 0;
+	Object.defineProperty(this, 'progress', {
+		get: function () {return progress;},
+		set: function (val) {
+			progress = +val || 0;
+			var progressEl = document.getElementById('limber-progress');
+			progressEl && progressEl.style.fontSize = progress + 'vw';
+		}
+	});
+}
